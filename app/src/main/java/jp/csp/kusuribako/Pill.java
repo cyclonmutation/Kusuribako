ce8cd0c68f7b87a7fca8e9666ca8fe051b92c066
cyclonmutation <cyclonmutation@gmail.com>
2017-12-17 21:56:56 +0900

    171217.Sun.2107 PillInputActivityにて画像を登録する修正中（Error）

--- app/src/main/java/jp/csp/kusuribako/Pill.java
+++ app/src/main/java/jp/csp/kusuribako/Pill.java
@@ -12,12 +12,12 @@ import io.realm.annotations.PrimaryKey;
 public class Pill extends RealmObject implements Serializable {
 
     private String mName;    //名前
-    private byte[] mBitmapArray;    //画像をbyte型の配列にしたもの
+    private String mImage;    //画像をString型の配列にしたもの
     private int mDosage;     //1回の服用量
     private String mUnit;    //1回の服用量の単位（袋、錠など）
     private String mFrequency;  //飲む頻度（毎日、特定の日、頓服など）
     private int mTimes;     //飲む回数（3回/日など）
-    private String mHour;       //飲む時間（8時、20時など）　←Alarm対象時刻
+    private ArrayList<String> mHourList;       //飲む時間（8時、20時など）　←Alarm対象時刻
 //    private boolean mLeftCheck;     //残量チェック要否
 //    private int mLeftVolume;        //残数（チェックが必要な場合のみ）
 
@@ -39,11 +39,11 @@ public class Pill extends RealmObject implements Serializable {
         this.mName = name;
     }
 
-    public byte[] getImageBytes() {
-        return mBitmapArray;
+    public String getImage() {
+        return mImage;
     }
-    public void setImageBytes(byte[] bytes) {
-        this.mBitmapArray = bytes;
+    public void setImageBytes(String img) {
+        this.mImage = img;
     }
 
     public int getDosage(){
@@ -74,11 +74,11 @@ public class Pill extends RealmObject implements Serializable {
         this.mTimes = mTimes;
     }
 
-    public String getmHour(){
-        return mHour;
+    public ArrayList<String>  getmHour(){
+        return mHourList;
     }
-    public void setmHour(String mHour){
-        this.mHour = mHour;
+    public void setmHour(ArrayList<String>  hourList){
+        this.mHourList = hourList;
     }
 
 //    public boolean getmLeftCheck(){
@@ -94,16 +94,19 @@ public class Pill extends RealmObject implements Serializable {
 //    public void setmLeftVolume(int mLeftVolume){
 //        this.mLeftVolume = mLeftVolume;
 //    }
+//
 
-//    public Pill(String name, byte[] bytes, int dosage, String unit, String frequency, int times, String hour, boolean leftcheck, int leftVolume) {
+
+//    public Pill(String name, String img, int dosage, String unit, String frequency, int times, ArrayList<String> hourlist) {
 //        mName = name;
-//        mBitmapArray = bytes;
+//        mImage = img;
 //        mDosage = dosage;
 //        mUnit = unit;
 //        mFrequency = frequency;
 //        mTimes = times;
-//        mHour = hour;
-//        mLeftCheck = leftcheck;
-//        mLeftVolume = leftVolume;
+//        mHourList = hourlist;
+////        mLeftCheck = leftcheck;
+////        mLeftVolume = leftVolume;
 //    }
+
 }
